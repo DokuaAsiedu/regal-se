e5924553cfe307b28a96c1e963c66e1f9ffeed11
DokuaAsiedu <asiedudokua@gmail.com>
2025-06-26 23:15:10 +0000

    initial commit

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('regal.se#2711'),
+            'email_verified_at' => now(),
+        ]);
+    }
+}
