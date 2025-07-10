0266fd602b17fb41c6f77f716183e42d84a24ca5
DokuaAsiedu <asiedudokua@gmail.com>
2025-07-10 23:28:29 +0000

    feat: show products on homepage

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Services\ProductService;
+use App\Services\StoreSettingsService;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    protected $productService;
+    protected $storeSettingsService;
+
+    public function __construct(ProductService $productService, StoreSettingsService $storeSettingsService)
+    {
+        $this->productService = $productService;
+        $this->storeSettingsService = $storeSettingsService;
+    }
+
     public function index()
     {
-        return view('client.index');
+        $products = $this->productService->products()->get();
+        $currency = $this->storeSettingsService
+            ->allQuery(['code' => 'currency_symbol'])
+            ->first()
+            ->value;
+
+        return view('client.index', compact('products', 'currency'));
     }
 }
--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,3 +1,7 @@
 <x-layouts.client>
-    Hello index
+    <div class="grid sm:grid-cols-3 gap-4">
+        @foreach ($products as $item)
+            <x-client.product-card class="rounded-lg border-2 border-orange-200" :name="$item->name" :price="$item->selling_price" :description="$item->description" :currency="$currency" />
+        @endforeach
+    </div>
 </x-layouts.client>
