f7681170e7e4721ae3bd58ed62a6a1ba278d2bbe
DokuaAsiedu <asiedudokua@gmail.com>
2025-07-14 02:05:46 +0000

    change: remove global active scope from product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
-use App\Models\Scopes\ActiveScope;
-use Illuminate\Database\Eloquent\Attributes\ScopedBy;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[ScopedBy([ActiveScope::class])]
 class Product extends Model
 {
     use HasFactory;
@@ -25,5 +24,19 @@ class Product extends Model
     public function status()
     {
         return $this->belongsTo(Status::class);
+    }
+
+    public function scopeActive(Builder $query)
+    {
+        $active_status_id = Status::where('code', 'active')
+            ->value('id');
+
+        return $query->where('status_id', $active_status_id);
+    }
+
+    #[Scope]
+    public function scopeQuantityAvailable(Builder $query)
+    {
+        return $query->where('quantity', '>', '0');
     }
 }
