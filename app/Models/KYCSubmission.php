0c8c7e404f6f9046c4cc7d0a753ba944977a863e
Dokua <akua.dokua@imedicaf.com>
2025-07-29 11:58:02 +0000

    feat: add user relation to kyc submission model

--- app/Models/KYCSubmission.php
+++ app/Models/KYCSubmission.php
@@ -44,4 +44,9 @@ class KYCSubmission extends Model
     {
         return $this->belongsTo(Status::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
