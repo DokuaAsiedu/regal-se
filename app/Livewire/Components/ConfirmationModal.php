48c2c98be8349c0b8c892fdcac6646a3efc19601
Dokua <akua.dokua@imedicaf.com>
2025-07-30 12:33:10 +0000

    feat: add conditional text input to confirmation modal

--- app/Livewire/Components/ConfirmationModal.php
+++ app/Livewire/Components/ConfirmationModal.php
@@ -18,6 +18,11 @@ class ConfirmationModal extends ModalComponent
     public $confirm_text;
     public $reason;
     public $show_input;
+    public $input_label;
+
+    protected $rules = [
+        'reason' => 'required|string',
+    ];
 
     /**
      * Supported: 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl'
@@ -27,12 +32,13 @@ class ConfirmationModal extends ModalComponent
         return 'md';
     }
 
-    public function mount($id, $event, $showInput = false, $title = null, $sub_title = null, $confirmText = null, $icon = null)
+    public function mount($id, $event, $showInput = false, $inputLabel = null, $title = null, $sub_title = null, $confirmText = null, $icon = null)
     {
         $this->id = $id;
         $this->event = $event;
         $this->show_input = $showInput;
         $this->title = $title ?? __('Are you sure?');
+        $this->input_label = $inputLabel ?? __('Reason');
         $this->sub_title = $sub_title ?? __('This action cannot be reversed');
         $this->confirm_text = $confirmText ?? ('Confirm');
         $this->icon = $icon ?? 'exclamation-triangle';
@@ -40,6 +46,8 @@ class ConfirmationModal extends ModalComponent
 
     public function confirm()
     {
+        if ($this->show_input) $this->validate();
+
         $this->dispatch($this->event, id: $this->id, reason: $this->reason);
     }
 
--- resources/views/livewire/components/confirmation-modal.blade.php
+++ resources/views/livewire/components/confirmation-modal.blade.php
@@ -3,16 +3,19 @@
     <flux:heading size="lg">{{ $title }}</flux:heading>
     <flux:text>{{ $sub_title }}</flux:text>
     <div class="flex flex-col items-center gap-2">
-    @if ($show_input)
-        <div class="flex items-center gap-2">
-            <label for="reason" class="text-gray-500">{{ __('Reason') }}:</label>
-            <flux:input id="reason" class="h-10" wire:model="reason" />
+        @if ($show_input)
+            <div class="flex items-center gap-2">
+                <label for="reason" class="text-gray-500">{{ $input_label }}:</label>
+                <div class="flex flex-col gap-1">
+                    <flux:input id="reason" class="h-10" wire:model="reason" />
+                    <flux:error name="reason" />
+                </div>
+            </div>
+        @endif
+        <div class="flex gap-2">
+            <flux:button wire:click="$dispatch('closeModal')" variant="filled">{{ __('Cancel') }}</flux:button>
+            <flux:button type="button" variant="danger" wire:click="confirm">{{ $confirm_text }}</flux:button>
         </div>
-    @endif
-    <div class="flex gap-2">
-        <flux:button wire:click="$dispatch('closeModal')" variant="filled">{{ __('Cancel') }}</flux:button>
-        <flux:button type="button" variant="danger" wire:click="confirm">{{ $confirm_text }}</flux:button>
-    </div>
     </div>
 </x-card>
 {{-- Success is as dangerous as failure. --}}
