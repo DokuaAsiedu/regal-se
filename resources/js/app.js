45c7a2a5d10bc88ab8ffd695fb71a0351b7089b2
Dokua <akua.dokua@imedicaf.com>
2025-08-25 04:23:50 +0000

    build: install chart.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,7 @@ import "./../../vendor/power-components/livewire-powergrid/dist/powergrid";
 import intlTelInput from "intl-tel-input";
 window.intlTelInput = intlTelInput;
 import "./custom.js";
+
+import Chart from 'chart.js/auto';
+
+window.Chart = Chart;
