a7272704e819ccdac3f65302b0ea3481a773fcec
Dokua <akua.dokua@imedicaf.com>
2025-08-25 04:24:13 +0000

    feat: show charts on dashboard

--- app/View/Composers/AdminDashboardComposer.php
+++ app/View/Composers/AdminDashboardComposer.php
@@ -5,6 +5,10 @@ namespace App\View\Composers;
 use App\Models\KYCSubmission;
 use App\Models\Order;
 use App\Models\Payment;
+use App\Models\User;
+use Carbon\Carbon;
+use DateInterval;
+use DatePeriod;
 use Illuminate\View\View;
 
 class AdminDashboardComposer
@@ -54,10 +58,64 @@ class AdminDashboardComposer
             ],
         ];
 
+        $orders_last_30_days = $this->getDataFromGivenPeriod(Order::class);
+        $payments_last_30_days = $this->getDataFromGivenPeriod(Payment::class);
+        $kycs_last_30_days = $this->getDataFromGivenPeriod(KYCSubmission::class);
+        $users_last_30_days = $this->getDataFromGivenPeriod(User::class);
+        $chart_data = [
+            [
+                'chart_cont_id' => 'orders-chart-cont',
+                'header' => __('Orders'),
+                'data' => $orders_last_30_days,
+            ],
+            [
+                'chart_cont_id' => 'kycs-chart-cont',
+                'header' => __('KYC Submissions'),
+                'data' => $kycs_last_30_days,
+            ],
+            [
+                'chart_cont_id' => 'payments-chart-cont',
+                'header' => __('Payments'),
+                'data' => $payments_last_30_days,
+            ],
+            [
+                'chart_cont_id' => 'users-chart-cont',
+                'header' => __('Users'),
+                'data' => $users_last_30_days,
+            ],
+        ];
+
         $data = [
             'quick_stats' => $quick_stats,
             'orders_today' => $orders_today,
+            'chart_data' => $chart_data,
         ];
         $view->with($data);
+    }
+
+    public function getDataFromGivenPeriod($model, $period = 30)
+    {
+        $data_over_given_period = $model::query()
+            ->selectRaw('DATE(created_at) as date, COUNT(*) as total')
+            ->where('created_at', '>=', Carbon::now()->subDays(30))
+            ->groupBy('date')
+            ->orderBy('date')
+            ->get();
+        $start_date = $data_over_given_period->min(fn ($elem) => Carbon::parse($elem['date']));
+        $end_date = $data_over_given_period->max(fn ($elem) => Carbon::parse($elem['date']));
+        $interval = DateInterval::createFromDateString('1 day');
+        $period = new DatePeriod($start_date, $interval, $end_date->copy()->addDay());
+
+        $res = [];
+        foreach ($period as $date) {
+            $formatted_date = $date->format('Y-m-d');
+            $record = $data_over_given_period->firstWhere('date', $formatted_date);
+            $res[] = [
+                'date' => $formatted_date,
+                'total' => $record ? $record->total : 0,
+            ];
+        }
+
+        return $res;
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,7 +15,8 @@
             @endforeach
         </div>
         <div class="grow grid lg:grid-cols-3 gap-4 overflow-hidden">
-            <div class="h-full max-lg:max-h-screen flex flex-col border-1 border-zinc-200 rounded-md shadow-lg overflow-hidden">
+            <div
+                class="h-full max-lg:max-h-screen flex flex-col border-1 border-zinc-200 rounded-md shadow-lg overflow-hidden">
                 <div class="p-4 border-b-2 border-b-zinc-200">
                     <flux:heading level="2" size="lg">{{ __('Orders today') }}</flux:heading>
                 </div>
@@ -27,11 +28,15 @@
                                 <flux:heading level="3" size="base">{{ $order->code }}</flux:heading>
                                 <div class="flex flex-wrap gap-2">
                                     <x-status :status="$order->status" />
-                                    <flux:badge color="purple">{{ $order->orderItems->count() . ' ' . Str::plural(__('item'), $order->orderItems->count()) }}</flux:badge>
+                                    <flux:badge color="purple">
+                                        {{ $order->orderItems->count() . ' ' . Str::plural(__('item'), $order->orderItems->count()) }}
+                                    </flux:badge>
                                 </div>
                             </div>
                             <div class="flex flex-col justify-center">
-                                <flux:button icon="eye" variant="filled" :href="route('orders.show', ['order' => $order->id])">{{ __('View') }}</flux:button>
+                                <flux:button icon="eye" variant="filled"
+                                    :href="route('orders.show', ['order' => $order->id])">{{ __('View') }}
+                                </flux:button>
                             </div>
                         </div>
                     @empty
@@ -41,6 +46,77 @@
                     @endforelse
                 </div>
             </div>
+            <div class="lg:col-span-2 p-4 flex flex-col gap-3">
+                <flux:heading level="2" size="lg">{{ __('Stats over the last 30 days') }}</flux:heading>
+                <div class="h-full grid grid-cols-2 gap-4">
+                    @foreach ($chart_data as $elem)
+                        <div class="p-4 flex flex-col gap-2 border-2 border-zinc-200 rounded-lg shadow-lg">
+                            <flux:heading level="4">{{ $elem['header'] }}</flux:heading>
+                            <div class="w-full grow">
+                                <canvas id="{{ $elem['chart_cont_id'] }}"></canvas>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
     </div>
+    <script>
+        function loadCharts() {
+            const charts = @json($chart_data);
+
+            charts.forEach(chart => {
+                const ctx = document.getElementById(chart.chart_cont_id);
+
+                new window.Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: chart.data.map(item => item.date),
+                        datasets: [{
+                            label: chart.header,
+                            data: chart.data.map(item => item.total),
+                            borderColor: 'rgba(59, 130, 246, 1)',
+                            borderWidth: 1,
+                            fill: false,
+                            tension: 0.3,
+                            pointBackgroundColor: 'rgba(59, 130, 246, 1)',
+                            pointRadius: 1,
+                        }],
+                    },
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        plugins: {
+                            legend: {
+                                display: false
+                            },
+                        },
+                        scales: {
+                            x: {
+                                // display: false,
+                                grid: {
+                                    display: false
+                                },
+                                ticks: {
+                                    display: false
+                                },
+                            },
+                            y: {
+                                grid: {
+                                    display: false
+                                },
+                                beginAtZero: true,
+                                ticks: {
+                                    precision: 0
+                                },
+                            },
+                        }
+                    },
+                });
+            });
+        }
+        window.addEventListener("DOMContentLoaded", function() {
+            loadCharts()
+        })
+    </script>
 </x-layouts.admin>
