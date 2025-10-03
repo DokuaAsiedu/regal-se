2693730c8649a640a53bd41153b7683898230890
Dokua <akua.dokua@imedicaf.com>
2025-10-03 19:43:17 +0000

    feat: accommodate for database unique constraints violations in exception handler

--- app/Traits/HandlesErrorMessage.php
+++ app/Traits/HandlesErrorMessage.php
@@ -6,6 +6,7 @@ use App\Exceptions\CustomException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
@@ -31,6 +32,9 @@ trait HandlesErrorMessage
         }
         if ($exception instanceof AuthorizationException) {
             return $this->responseForAuthorizationException($exception);
+        }
+        if ($exception instanceof QueryException) {
+            return $this->responseForQueryException($exception);
         }
         if ($exception instanceof CustomException) {
             return $this->responseForCustomException($exception);
@@ -96,6 +100,37 @@ trait HandlesErrorMessage
         $res = new stdClass();
         $res->message = $exception->getMessage();
         $res->code = Response::HTTP_BAD_REQUEST;
+
+        return $res;
+    }
+
+    public function responseForQueryException(QueryException $e)
+    {
+        $errorCode = $e->errorInfo[1] ?? null;
+        $driverMessage = $e->errorInfo[2] ?? null;
+        logger('query error', [$errorCode, $driverMessage]);
+
+        switch ($errorCode) {
+            case 1062: // Duplicate entry
+                if (preg_match("/for key '(.+?)'/", $driverMessage, $matches)) {
+                    $indexName = $matches[1];
+                    $str = str_replace(['_unique'], '', $indexName);
+                    $table_name = explode(".", $str)[0];
+                    $field = str_replace([$table_name, '.', '_'], ' ', $str);
+
+                    $error_message = "Duplicate entry for $field";
+                } else {
+                    $error_message = "Duplicate entry detected";
+                }
+                $message = $error_message;
+                break;
+            default:
+                $message = 'An error occured';
+        }
+
+        $res = new stdClass();
+        $res->message = $message;
+        $res->code = Response::HTTP_INTERNAL_SERVER_ERROR;
 
         return $res;
     }
