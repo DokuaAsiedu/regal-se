90c4ffa13202fb0201ada7dbc40b895b68397d64
Dokua <akua.dokua@imedicaf.com>
2025-10-27 18:26:34 +0000

    build: configure spatie media library

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,10 +8,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
-    use HasFactory, LogsActivity;
+    use HasFactory, LogsActivity, InteractsWithMedia;
 
     protected $fillable = [
         'code',
@@ -28,6 +32,14 @@ class Product extends Model
         return LogOptions::defaults()
         ->logOnly(['*']);
         // Chain fluent methods for configuration options
+    }
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->fit(Fit::Contain, 300, 300)
+            ->nonQueued();
     }
 
     public function status()
